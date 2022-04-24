f77edb9e43b926f02657936193ca20d781c8f513
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-04-24 18:37:55 -0300

    Adicionando mais rotas (get by id, post, put e delete).

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 router
 .get("/livros", LivroController.listarLivros)
-
-
+.get("/livros/:id", LivroController.listarLivrosPorId)
+.post("/livros", LivroController.cadastrarLivro )
+.put("livros/:id", LivroController.atualizarLivro)
+.delete("livros/:id", LivroController.excluirLivro)
 
 module.exports = router
