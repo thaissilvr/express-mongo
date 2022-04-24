1c39fc9003364c668ef6536762875794e7cbfb7f
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-04-24 17:51:32 -0300

    Modelando o schema do banco de dados.

---
+++ src/models/Livro.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const livrosSchema = new mongoose.Schema(
+    {
+        id: {type: String},
+        titulo: {type: String, required: true},
+        autor: {type: String, required: true},
+        editora: {type: String, required: true},
+        numeroPaginas: {type: Number}
+    }
+);
+
+const livros = mongoose.model("livros", livrosSchema)
+
+module.exports = livros;
