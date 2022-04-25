30d3cb39ede678cb4d4ca3652f3cbfdb8bb7d0c1
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-04-25 11:51:21 -0300

    Habilitando busca de livro por editora.

--- src/controller/LivrosController.js
+++ src/controller/LivrosController.js
@@ -30,7 +30,6 @@ class LivroController {
             }
         })
     }
-
     static atualizarLivro = (req, res) =>{
         const id = req.params.id;
         livros.findByIdAndUpdate(id, {$set : req.body}, (err) =>{
@@ -50,6 +49,12 @@ class LivroController {
             } else {
                 res.status(500).send({message: err.message})
             }
+        })
+    }
+    static listarLivroPorEditora = (req, res) => {
+        const editora = req.query.editora
+        livros.find({"editora": editora}, {}, (err, livros) => {
+            res.status(200).send(livros);
         })
     }
 }
